15eed79f1305dc017536d30c5e3c0dd9965c0642
ashishkumarsingh296@gmail.com <ashishkumarsingh296@gmail.com>
2025-11-15 14:50:30 +0530

    permitting url to config security

--- src/main/java/com/example/forinterviewpracticespringbootalltopicimplementaion/configuration/SecurityConfig.java
+++ src/main/java/com/example/forinterviewpracticespringbootalltopicimplementaion/configuration/SecurityConfig.java
@@ -40,6 +40,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/health").permitAll()
+                        .requestMatchers("/actuator/info").permitAll()
                         .requestMatchers(
                                 "/auth/**",
                                 "/swagger-ui/**",
