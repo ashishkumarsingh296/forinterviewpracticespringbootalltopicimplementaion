db310c721f0ff8592a3bfc7183cc45bbd2ee7a35
ashishkumarsingh296@gmail.com <ashishkumarsingh296@gmail.com>
2025-12-10 02:03:01 +0530

    commiting for order modulesk

--- src/main/java/com/example/forinterviewpracticespringbootalltopicimplementaion/dto/OrderResponseDTO.java
+++ src/main/java/com/example/forinterviewpracticespringbootalltopicimplementaion/dto/OrderResponseDTO.java
@@ -1,5 +1,6 @@
 package com.example.forinterviewpracticespringbootalltopicimplementaion.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,6 +13,7 @@ public class OrderResponseDTO {
     private String orderNumber;
     private Double totalAmount;
     private String status;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
     private List<OrderItemDTO> items;
     private PaymentDTO payment;
