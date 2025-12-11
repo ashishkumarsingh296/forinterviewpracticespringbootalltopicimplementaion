61e2ec449c6da31fa34fbaf5bac571af2e21275d
ashishkumarsingh296@gmail.com <ashishkumarsingh296@gmail.com>
2025-12-11 22:08:59 +0530

    coomitting new modul order and paymenthqs

--- src/main/java/com/example/forinterviewpracticespringbootalltopicimplementaion/mapper/WalletMapper.java
+++ src/main/java/com/example/forinterviewpracticespringbootalltopicimplementaion/mapper/WalletMapper.java
@@ -1,10 +1,29 @@
 package com.example.forinterviewpracticespringbootalltopicimplementaion.mapper;
 
+import com.example.forinterviewpracticespringbootalltopicimplementaion.dto.OrderDTO;
+import com.example.forinterviewpracticespringbootalltopicimplementaion.dto.OrderItemDTO;
 import com.example.forinterviewpracticespringbootalltopicimplementaion.dto.WalletDTO;
+import com.example.forinterviewpracticespringbootalltopicimplementaion.entity.Order;
+import com.example.forinterviewpracticespringbootalltopicimplementaion.entity.OrderItem;
 import com.example.forinterviewpracticespringbootalltopicimplementaion.entity.Wallet;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = "spring")
-public interface WalletMapper {
-    WalletDTO toDTO(Wallet wallet);
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class WalletMapper {
+
+    public WalletDTO toDTO(WalletDTO wallet) {
+        if (wallet == null) return null;
+
+        WalletDTO dto = new WalletDTO();
+        dto.setWalletId(wallet.getWalletId());
+        dto.setUserId(wallet.getUserId() != null ? wallet.getUserId() : null);
+        dto.setBalance(wallet.getBalance());
+
+        return dto;
+    }
+
 }
