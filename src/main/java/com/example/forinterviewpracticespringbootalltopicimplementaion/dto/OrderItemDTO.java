b3d17ea3301eb7e5ff1063ec270545b50040f367
ashishkumarsingh296@gmail.com <ashishkumarsingh296@gmail.com>
2025-12-11 19:48:59 +0530

    coomitting new modul order and paymentasasd

--- src/main/java/com/example/forinterviewpracticespringbootalltopicimplementaion/dto/OrderItemDTO.java
+++ src/main/java/com/example/forinterviewpracticespringbootalltopicimplementaion/dto/OrderItemDTO.java
@@ -1,10 +1,14 @@
 package com.example.forinterviewpracticespringbootalltopicimplementaion.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderItemDTO {
     private Long productId;
     private String productName;
