84b14794df9b643f610b4143237e3b0bd6e03f75
ashishkumarsingh296@gmail.com <ashishkumarsingh296@gmail.com>
2025-12-14 00:25:39 +0530

    commiting for invice realted changesaw

--- src/main/java/com/example/forinterviewpracticespringbootalltopicimplementaion/entity/PaymentEvent.java
+++ src/main/java/com/example/forinterviewpracticespringbootalltopicimplementaion/entity/PaymentEvent.java
@@ -1,15 +1,15 @@
 package com.example.forinterviewpracticespringbootalltopicimplementaion.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor   // 🔥 REQUIRED FOR JACKSON
+@AllArgsConstructor
 @Data
 public class PaymentEvent {
 
     private String orderId;
     private String eventType;
 
-    public PaymentEvent(String orderId, String eventType) {
-        this.orderId = orderId;
-        this.eventType = eventType;
-    }
 }
