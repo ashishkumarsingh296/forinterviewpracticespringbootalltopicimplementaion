35f9b6794df1578fe49005abf0d29f35e32c0d09
ashishkumarsingh296 <62248672+ashishkumarsingh296@users.noreply.github.com>
2025-12-13 19:21:31 +0530

    Update WalletRepository.java

--- src/main/java/com/example/forinterviewpracticespringbootalltopicimplementaion/repository/WalletRepository.java
+++ src/main/java/com/example/forinterviewpracticespringbootalltopicimplementaion/repository/WalletRepository.java
@@ -1,18 +1,16 @@
 package com.example.forinterviewpracticespringbootalltopicimplementaion.repository;
 
 import com.example.forinterviewpracticespringbootalltopicimplementaion.entity.Wallet;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.persistence.LockModeType;
 import java.util.Optional;
 
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
-    Optional<Wallet> findTopByUserIdOrderByIdDesc(String userId);
+    Optional<Wallet> findTopByUserIdForUpdate(String userId);
 }
